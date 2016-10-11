68174e33130bb3e08ee4760c08c85d22ad51eeb6
jmsr <jmarcosuarez@gmail.com>
2016-10-11 11:44:04 +0100

    Set eslint - start script

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
-var webpack = require('webpack'); //require the webpack lib
+var webpack = require('webpack'); //  require the webpack lib
 
 module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080', // Setting the URL for the hot reload
     'webpack/hot/only-dev-server', // Reload only the dev server
-    './src/index.js'
+    './src/index.js',
   ],
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader', // Include the react-hot babel
-    }]
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader', 'eslint-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['', '.js', '.jsx'],
   },
   output: {
     path: __dirname + './dist',
     publicPath: '/',
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   devServer: {
     contentBase: './dist',
-    hot: true // Activate hot loading
+    hot: true, // Activate hot loading
+  },
+  eslint: {
+    configFile: './.eslintrc',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin() // Wire in the hot replacement plugin
-  ]
+    new webpack.HotModuleReplacementPlugin(), // Wire in the hot replacement plugin
+  ],
 };
